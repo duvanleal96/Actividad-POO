af6e1e7df546910119f5c5ecba5d506443832a11
duvanleal96 <duvanleal65@gmail.com>
2022-05-27 20:51:31 -0500

    se añade documentacion en Message

--- taller/src/main/java/com/sofka/software/Message.java
+++ taller/src/main/java/com/sofka/software/Message.java
@@ -2,13 +2,12 @@ package com.sofka.software;
 
 import java.util.logging.Logger;
 
+/**
+ * clase mensaje, imprementa los metodos de mostrar mensaje y respuesta de informacion
+ */
 public class Message {
     private static final Logger logger = Logger.getLogger(String.valueOf(Message.class));
-
-    public static Message getInstance() {
-        return new Message();
-    }
-
+    
     public void dogAction() {
         logger.info("guau");
     }
